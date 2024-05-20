80be9f01771db2b6eafc7081964ac2517d2fd8d2
xiaowuzxc <wuyan-100@163.com>
2024-05-20 10:43:56 +0800

    1

--- src/project.v
+++ src/project.v
@@ -15,13 +15,13 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-
+reg [7:0]sft_reg;
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = ui_in + sft_reg;
   assign uio_oe  = sft_reg;
 
-reg [7:0]sft_reg;
+
   always @(posedge clk) begin
     sft_reg[7:0] <= {sft_reg[6:0],ui_in[0]}
   end

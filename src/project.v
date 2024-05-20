f811fff5edd8001c53b455849c1f78b2ed336d20
xiaowuzxc <wuyan-100@163.com>
2024-05-20 10:35:17 +0800

    test

--- src/project.v
+++ src/project.v
@@ -18,7 +18,12 @@ module tt_um_example (
 
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uio_out = ui_in + sft_reg;
+  assign uio_oe  = sft_reg;
+
+reg [7:0]sft_reg;
+  always @(posedge clk) begin
+    sft_reg[7:0] <= {sft_reg[6:0],ui_in[0]}
+  end
 
 endmodule
